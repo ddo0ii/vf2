67237a0c1588a3e36fb301ad867e098da87810e9
soyhandong <21500035@handong.edu>
2022-03-18 18:15:32 +0900

    Navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>이게앱바지</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn icon to="/about">
@@ -16,6 +16,40 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-content>
       <router-view/>
@@ -40,7 +74,13 @@
 export default {
   name: 'App',
   data: () => ({
-
-  })
+    drawer: false,
+    group: null
+  }),
+  watch: {
+    group () {
+      this.drawer = false
+    }
+  }
 }
 </script>
