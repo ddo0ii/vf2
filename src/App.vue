26b8d8a5d6b8f200370dc48515adf8e42f00c0ad
soyhandong <21500035@handong.edu>
2022-03-22 08:48:08 +0900

    component 사용 정리

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,31 @@
 <template>
   <v-app>
-    <!-- app이라고 항상 언급 해야함 -->
     <v-app-bar
       app
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <site-title :title="title"></site-title>
-      <v-spacer></v-spacer>
-      <v-btn icon to="/about">
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon to="/">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      <v-spacer/>
     </v-app-bar>
-
     <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">Application</v-list-item-title>
-          <v-list-item-subtitle>subtext</v-list-item-subtitle>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
       <site-menu></site-menu>
     </v-navigation-drawer>
-
     <v-content>
       <router-view/>
     </v-content>
-    <!-- <v-main>
-      <router-view/>
-    </v-main> -->
-    <!-- vue에서 v-content랑 v-main을 넣으면 되는듯 근데 저자는 main을 썼음 -->
     <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
@@ -47,16 +38,15 @@ import SiteMenu from '@/components/site/menu'
 export default {
   components: { SiteTitle, SiteFooter, SiteMenu },
   name: 'App',
-  data: () => ({
-    drawer: false,
-    title: '나의 타이틀입니다.',
-    footer: '푸터입니다.',
-    group: null
-  }),
-  watch: {
-    group () {
-      this.drawer = false
+  data () {
+    return {
+      drawer: false,
+      items: [],
+      title: '나의 타이틀입니다',
+      footer: '푸터입니다'
     }
+  },
+  mounted () {
   }
 }
 </script>
--- src/components/site/menu.vue
+++ src/components/site/menu.vue
@@ -1,24 +1,27 @@
 <template>
-<div>
+  <div>
     <v-list-item v-for="item in items" :key="item.title" :to="item.to">
-        <v-list-item-content>
-            {{ item.title }}
-        </v-list-item-content>
+      <v-list-item-content>
+        {{ item.title }}
+      </v-list-item-content>
     </v-list-item>
-</div>
+  </div>
 </template>
 <script>
 export default {
+  props: ['footer'],
   data () {
     return {
       items: [
         {
           title: 'home',
           to: '/'
-        }, {
+        },
+        {
           title: 'about',
           to: '/about'
-        }, {
+        },
+        {
           title: 'wow',
           to: '/wow'
         }
