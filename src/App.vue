d8755b02f01682616f0aecf4ae8472036d72f3b5
soyhandong <21500035@handong.edu>
2022-03-22 09:10:28 +0900

    메뉴 그룹화하기

--- src/App.vue
+++ src/App.vue
@@ -10,17 +10,6 @@
       <v-spacer/>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Application
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
       <site-menu></site-menu>
     </v-navigation-drawer>
     <v-content>
