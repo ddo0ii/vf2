6bb5ff5ee0929ec9ac224de5581c90ebd057da86
soyhandong <21500035@handong.edu>
2022-03-24 11:32:03 +0900

    realtime DB 설명까지 추가

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <site-title :title="title"></site-title>
       <v-spacer/>
-      <v-btn icon @click="write"><v-icon>mdi-check</v-icon></v-btn>
+      <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
       <v-btn icon @click="read"><v-icon>mdi-numeric</v-icon></v-btn>
       <v-btn icon @click="readOne"><v-icon>mdi-account-check</v-icon></v-btn>
     </v-app-bar>
@@ -41,18 +41,22 @@ export default {
     console.log(this.$firebase)
   },
   methods: {
-    write () {
-      console.log('save@@@')
+    save () {
+      console.log('save')
+      // 계속 child를 붙여서 넣어도 됨
       this.$firebase.database().ref().child('abcd').set({
         title: 'abcd', text: 'tttttt'
       })
     },
+    // 이거는 DB에서 자체적으로 계속 바꾸면 바뀌는대로 console에 출력된다. 계속 변화 감지
     read () {
       this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+        // sn은 snapshot임
         console.log(sn)
         console.log(sn.val())
       })
     },
+    // 얘는 DB에서 바꿔도 다시 출력되지 않는다. 일회성 느낌. 순차적으로 할때 아래와 같이. then 붙여서 많이 함
     async readOne () {
       const sn = await this.$firebase.database().ref().child('abcd').once('value')
       console.log(sn.val())
