c3384ff07d56fd3f51b889969d1e3a2360f5e2ef
soyhandong <21500035@handong.edu>
2022-03-28 13:31:34 +0900

    하위메뉴추가하기

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    editable: true
   },
   getters: {
   },
   mutations: {
+    setEdit (state, edit) {
+      state.editable = edit
+    }
   },
   actions: {
   },
