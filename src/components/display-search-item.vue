03cebf6eba1d289834157f0e7caf4e73d4571d12
soyhandong <21500035@handong.edu>
2022-05-11 16:49:33 +0900

    100 검색 내용 표시하기

---
+++ src/components/display-search-item.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-card outlined  class="mb-4">
+        <v-card-subtitle>
+            <display-title/>
+        </v-card-subtitle>
+    </v-card>
+</template>
+<script>
+
+</script>
